0de524c9ce61e8f2d39c03d5555f68126b526546
eugenek45 <eugenekiprotich0@gmail.com>
2020-10-13 15:42:36 +0300

    removed

--- EUgene/EUgene/settings.py
+++ EUgene/EUgene/settings.py
@@ -67,7 +67,7 @@ INSTALLED_APPS = [
 
   
     'bootstrap4',
-    'portfolio.apps.PortfolioConfig',
+    'EUgene.portfolio',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
